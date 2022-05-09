c3acf44a5831a2cbfa72ccd8bdc7675e8a29028f
Sean Roby <robywrote@gmail.com>
2022-05-09 14:20:00 -0500

    add hover function

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ const gridDimension = 2
 const gridSquares = gridDimension * gridDimension
 
 for (let i=0; i < gridSquares; i++) {
-    const gridContainer = document.getElementById("grid-container")
+    const gridContainer = document.getElementById('grid-container')
     const newDiv = document.createElement('div')
     newDiv.classList.add('item')
+    newDiv.addEventListener('mouseover', doOnMouseover)
     gridContainer.appendChild(newDiv)
 }
+
+function doOnMouseover(e) {
+    const item = e.target
+    item.classList.add('hover')
+  }
 
