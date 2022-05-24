3db9a4cef5874a4f2e04788198479e32fe4e8d26
Sean Roby <robywrote@gmail.com>
2022-05-24 02:02:31 -0500

    fix removeGrid

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 'use strict'
 
-const gridDimension = 2
-const gridSquares = gridDimension * gridDimension
+const button = document.querySelector('button')
+button.addEventListener('click', doOnClick)
 
-for (let i=0; i < gridSquares; i++) {
-    const gridContainer = document.getElementById('grid-container')
-    const newDiv = document.createElement('div')
-    newDiv.classList.add('item')
-    newDiv.addEventListener('mouseover', doOnMouseover)
-    gridContainer.appendChild(newDiv)
+function buildGrid(squares) {
+    for (let i=0; i < squares; i++) {
+        const gridContainer = document.getElementById('grid-container')
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('item')
+        newDiv.addEventListener('mouseover', doOnMouseover)
+        gridContainer.appendChild(newDiv)
+    }
+}
+
+function removeGrid() {
+    let items = document.querySelectorAll('.item')
+    let hovers = document.querySelectorAll('.hover')
+
+    items.forEach(e => e.remove())
+    hovers.forEach(e => e.remove())
 }
 
 function doOnMouseover(e) {
     const item = e.target
+    item.classList.remove('item')
     item.classList.add('hover')
+}
+
+function doOnClick(e) {
+    removeGrid()
+    let gridDimension = prompt('How many grid columns do you want?')
+    let gridSquares = gridDimension * gridDimension
+    buildGrid(gridSquares)
   }
+
 
